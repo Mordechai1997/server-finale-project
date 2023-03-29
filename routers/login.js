f11699db5439acc990472807b117208dab611073
mordechai1997 <mshukrun1997@gmail.com>
2023-03-29 20:54:06 +0300

    first commit

---
+++ routers/login.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const controllerSignin = require('../controllers/login')
+
+
+router.post('/login', controllerSignin.signin)
+router.post('/signup', controllerSignin.signup)
+router.get('/reset-password-email', controllerSignin.resetPasswordEmail)
+router.post('/update-password', controllerSignin.updatePassword)
+router.get('/getEmailUserByMemberId',controllerSignin.isAuthControllers ,controllerSignin.getEmailUserByMemberId)
+router.post('/updateUserDetails',controllerSignin.isAuthControllers ,controllerSignin.updateUserDetails)
+router.get('/auth', controllerSignin.isAuth)
+
+
+
+
+
+module.exports = router;
